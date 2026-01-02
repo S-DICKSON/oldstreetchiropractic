abac45d8249d9985eb6e8d759a06d2ea0d6758dc
Stephen Dickson <stephen.dickson@agentsoftware.net>
2026-01-02 17:17:13 +0000

    testing worker deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 
 // https://vite.dev/config/
@@ -12,6 +13,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    cloudflare({ entry: 'src/index.js' }),
   ],
   resolve: {
     alias: {
